ae351b905bf56968937eaee1f93727b0642a188d
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-02-17 16:11:53 +0000

    renamed obsolete meshers, cleaned the source code of meshfem2D_circular_canyon.f90, and added BibTeX information to the mesher and the solver.

--- SEM_2D_Dimitri/specfem2D.f90
+++ SEM_2D_Dimitri/specfem2D.f90
@@ -52,6 +52,27 @@
 ! Therefore, in order to have a Ricker source in pressure, use the first-derivative
 ! of a Gaussian for the time source for Chi in the parameter file.
 
+! If you use this code for your own research, please cite:
+!
+! @ARTICLE{KoTr99,
+! author={D. Komatitsch and J. Tromp},
+! year=1999,
+! title={Introduction to the spectral-element method for 3-{D} seismic wave propagation},
+! journal={Geophys. J. Int.},
+! volume=139,
+! number=3,
+! pages={806-822},
+! doi={10.1046/j.1365-246x.1999.00967.x}}
+!
+! @ARTICLE{KoVi98,
+! author={D. Komatitsch and J. P. Vilotte},
+! title={The spectral-element method: an efficient tool to simulate the seismic response of 2{D} and 3{D} geological structures},
+! journal={Bull. Seismol. Soc. Am.},
+! year=1998,
+! volume=88,
+! number=2,
+! pages={368-392}}
+
   program specfem2D
 
   implicit none
