ad11da0e6b0d32cdd95647493ae33759b1679cf5
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-04-17 09:48:49 +0000

    added program to create a Gaussian profile for topography or bathymetry

---
+++ UTILS/create_gaussian_profile.f90
@@ -0,0 +1,24 @@
+
+! create a gaussian profile for bathymetry
+
+  program create_gaussian_profile
+
+  implicit none
+
+  integer, parameter :: N = 200
+
+  double precision, parameter :: horiz_size = 10000.d0, x0 = horiz_size / 2.d0, sigma = horiz_size / 20.d0, height = 400.d0
+
+  double precision :: deltax,x
+
+  integer :: i
+
+  deltax = horiz_size / dble(N-1)
+
+  do i = 1,N
+    x = (i-1) * deltax
+    print *,x, -3000.d0 + height * exp(-(x-x0)**2/(2*sigma**2))
+  enddo
+
+ end program create_gaussian_profile
+
