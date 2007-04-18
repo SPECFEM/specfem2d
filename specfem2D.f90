28e9dcfbe8decf2afa50894a9b802c8676a6ff95
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-04-18 11:26:20 +0000

    added action='read' to open statement

--- specfem2D.f90
+++ specfem2D.f90
@@ -235,10 +235,10 @@
 !
 !***********************************************************************
 
-  open(IIN,file='OUTPUT_FILES/Database')
+  open(IIN,file='OUTPUT_FILES/Database',status='old',action='read')
 
 ! determine if we write to file instead of standard output
-  if(IOUT /= ISTANDARD_OUTPUT) open(IOUT,file='simulation_results.txt')
+  if(IOUT /= ISTANDARD_OUTPUT) open(IOUT,file='simulation_results.txt',status='unknown')
 
 !
 !---  read job title and skip remaining titles of the input file
