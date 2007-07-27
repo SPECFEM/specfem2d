a27487f97f273e2c5f21e7c3372763e512327e12
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-07-27 16:08:31 +0000

    added attenuation constants for Paul Cristini for fluid/solid ocean acoustics simulations

--- constants.h
+++ constants.h
@@ -100,7 +100,12 @@
 
 !-----------------------------------------------------------------------
 
-! attenuation constants from J. M. Carcione, Seismic modeling in viscoelastic media, Geophysics,
+! attenuation constants for standard linear solids
+! nu1 is the dilatation mode
+! nu2 is the shear mode
+! mech1 is the first standard linear solid, mech2 is the second
+
+! from J. M. Carcione, Seismic modeling in viscoelastic media, Geophysics,
 ! vol. 58(1), p. 110-120 (1993) for two memory-variable mechanisms (page 112).
 ! Beware: these values implement specific values of the quality factors:
 ! Qp approximately equal to 13 and Qs approximately equal to 10,
@@ -115,8 +120,8 @@
 ! double precision, parameter :: tau_epsilon_nu2_mech2 = 0.0029d0
 ! double precision, parameter :: tau_sigma_nu2_mech2   = 0.0024d0
 
-! attenuation constants from J. M. Carcione, D. Kosloff and R. Kosloff,
-! Wave propagation simulation in a linear viscoelastic medium, Geophysical Journal International,
+! from J. M. Carcione, D. Kosloff and R. Kosloff, Wave propagation simulation
+! in a linear viscoelastic medium, Geophysical Journal International,
 ! vol. 95, p. 597-611 (1988) for two memory-variable mechanisms (page 604).
 ! Beware: these values implement specific values of the quality factors:
 ! Qp approximately equal to 27 and Qs approximately equal to 20,
@@ -130,6 +135,39 @@
   double precision, parameter :: tau_sigma_nu1_mech2   = 0.0031146d0
   double precision, parameter :: tau_epsilon_nu2_mech2 = 0.0033257d0
   double precision, parameter :: tau_sigma_nu2_mech2   = 0.0030465d0
+
+! values for Paul Cristini for fluid-solid ocean acoustics simulations
+
+! frequency range: 3.000000 Hz - 20.000000 Hz
+! central frequency in log scale in Hz = 7.745966692414834
+! target constant attenuation factor Q = 136.4376068115
+
+! tau sigma evenly spaced in log frequency, do not depend on value of Q
+! double precision, parameter :: tau_sigma_nu1_mech1   = 0.05305164769729849711d0
+! double precision, parameter :: tau_sigma_nu1_mech2   = 0.00795774715459477387d0
+
+! double precision, parameter :: tau_epsilon_nu1_mech1 = 0.05361741010508015715d0
+! double precision, parameter :: tau_epsilon_nu1_mech2 = 0.00804740719550106794d0
+
+! frequency range: 1.500000 Hz - 18.000000 Hz
+! central frequency in log scale in Hz = 5.196152422706633
+! target constant attenuation factor Q = 136.4376068115
+
+! tau sigma evenly spaced in log frequency, do not depend on value of Q
+! double precision, parameter :: tau_sigma_nu1_mech1 = 0.10610329539459699422d0
+! double precision, parameter :: tau_sigma_nu1_mech2 = 0.00884194128288308401d0
+
+! double precision, parameter :: tau_epsilon_nu1_mech1 = 0.10754721280605997191d0
+! double precision, parameter :: tau_epsilon_nu1_mech2 = 0.00895488050110176612d0
+
+! double precision, parameter :: tau_epsilon_nu2_mech1 = tau_epsilon_nu1_mech1
+! double precision, parameter :: tau_epsilon_nu2_mech2 = tau_epsilon_nu1_mech2
+! double precision, parameter :: tau_sigma_nu2_mech1   = tau_sigma_nu1_mech1
+! double precision, parameter :: tau_sigma_nu2_mech2   = tau_sigma_nu1_mech2
+
+!
+!--- other constants computed from the parameters above, do not modify
+!
 
   double precision, parameter :: inv_tau_sigma_nu1_mech1 = ONE / tau_sigma_nu1_mech1
   double precision, parameter :: inv_tau_sigma_nu2_mech1 = ONE / tau_sigma_nu2_mech1
