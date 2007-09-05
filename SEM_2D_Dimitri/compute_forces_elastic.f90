0d996b387cac9efcbc5f7cb6b73f01b1f1ce7b46
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-09-05 11:52:17 +0000

    fixed small bug introduced by Nicolas Le Goff: "if" statement not correctly included in another "if" statement

--- SEM_2D_Dimitri/compute_forces_elastic.f90
+++ SEM_2D_Dimitri/compute_forces_elastic.f90
@@ -452,24 +452,22 @@
 
   endif  ! end of absorbing boundaries
 
-
 ! --- add the source
   if(.not. initialfield) then
 
-     if ( is_proc_source == 1 ) then
+! if this processor carries the source and the source element is elastic
+     if (is_proc_source == 1 .and. elastic(ispec_selected_source)) then
+
 ! collocated force
 ! beware, for acoustic medium, source is a potential, therefore source time function
 ! gives shape of velocity, not displacement
         if(source_type == 1) then
-           if(elastic(ispec_selected_source)) then
-              accel_elastic(1,iglob_source) = accel_elastic(1,iglob_source) - sin(angleforce)*source_time_function(it)
-              accel_elastic(2,iglob_source) = accel_elastic(2,iglob_source) + cos(angleforce)*source_time_function(it)
-           endif
+
+          accel_elastic(1,iglob_source) = accel_elastic(1,iglob_source) - sin(angleforce)*source_time_function(it)
+          accel_elastic(2,iglob_source) = accel_elastic(2,iglob_source) + cos(angleforce)*source_time_function(it)
 
 ! moment tensor
         else if(source_type == 2) then
-
-           if(elastic(ispec_selected_source)) then
 ! add source array
               do j=1,NGLLZ
                  do i=1,NGLLX
@@ -477,14 +475,14 @@
                     accel_elastic(:,iglob) = accel_elastic(:,iglob) + sourcearray(:,i,j)*source_time_function(it)
                  enddo
               enddo
-           endif
 
+        else
+          call exit_MPI('wrong source type in elastic element')
         endif
-     endif
 
-  else
-     call exit_MPI('wrong source type')
-  endif
+     endif ! if this processor carries the source and the source element is elastic
+
+  endif ! if not using an initial field
 
 ! implement attenuation
   if(TURN_ATTENUATION_ON) then
