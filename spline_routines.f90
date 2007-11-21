9147f20149eb277124ae14e853d3a5800baaaa3c
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-11-21 12:02:54 +0000

    renamed numerical_recipes.f90 to spline_routines.f90 to avoid copyright problem

--- spline_routines.f90
+++ spline_routines.f90
@@ -11,7 +11,7 @@
 ! compute spline coefficients (Numerical Recipes)
 ! modified to use dynamic allocation
 
-  subroutine spline(x,y,n,yp1,ypn,y2)
+  subroutine spline_construction(x,y,n,yp1,ypn,y2)
 
   implicit none
 
@@ -45,13 +45,13 @@
 
   deallocate(u)
 
-  end subroutine spline
+  end subroutine spline_construction
 
 ! --------------
 
 ! evaluate spline (adapted from Numerical Recipes)
 
-  subroutine splint(xa,ya,y2a,n,x,y)
+  subroutine spline_evaluation(xa,ya,y2a,n,x,y)
 
   implicit none
 
@@ -82,5 +82,5 @@
 
   Y = A*YA(KLO) + B*YA(KHI) + ((A**3-A)*Y2A(KLO) + (B**3-B)*Y2A(KHI))*(H**2)/6.d0
 
-  end subroutine splint
+  end subroutine spline_evaluation
 
