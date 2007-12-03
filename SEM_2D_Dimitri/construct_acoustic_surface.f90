9c7f3d9491fd084512d20f79a3b8b32d729304da
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-12-03 15:01:18 +0000

    added the CeCILL license

--- SEM_2D_Dimitri/construct_acoustic_surface.f90
+++ SEM_2D_Dimitri/construct_acoustic_surface.f90
@@ -4,14 +4,42 @@
 !                   S P E C F E M 2 D  Version 5.2
 !                   ------------------------------
 !
-!  Main authors: Dimitri Komatitsch, Nicolas Le Goff and Roland Martin
-!                 University of Pau and CNRS, France
+! Copyright Universite de Pau et des Pays de l'Adour and CNRS, France.
+! Contributors: Dimitri Komatitsch, dimitri DOT komatitsch aT univ-pau DOT fr
+!               Nicolas Le Goff, nicolas DOT legoff aT univ-pau DOT fr
+!               Roland Martin, roland DOT martin aT univ-pau DOT fr
 !
-!        (c) University of Pau and CNRS, France, November 2007
+! This software is a computer program whose purpose is to solve
+! the two-dimensional viscoelastic anisotropic wave equation
+! using a spectral-element method (SEM).
+!
+! This software is governed by the CeCILL license under French law and
+! abiding by the rules of distribution of free software. You can use,
+! modify and/or redistribute the software under the terms of the CeCILL
+! license as circulated by CEA, CNRS and INRIA at the following URL
+! "http://www.cecill.info".
+!
+! As a counterpart to the access to the source code and rights to copy,
+! modify and redistribute granted by the license, users are provided only
+! with a limited warranty and the software's author, the holder of the
+! economic rights, and the successive licensors have only limited
+! liability.
+!
+! In this respect, the user's attention is drawn to the risks associated
+! with loading, using, modifying and/or developing or reproducing the
+! software by the user in light of its specific status of free software,
+! that may mean that it is complicated to manipulate, and that also
+! therefore means that it is reserved for developers and experienced
+! professionals having in-depth computer knowledge. Users are therefore
+! encouraged to load and test the software's suitability as regards their
+! requirements in conditions enabling the security of their systems and/or
+! data to be ensured and, more generally, to use and operate it in the
+! same conditions as regards security.
+!
+! The full text of the license is available in file "LICENSE".
 !
 !========================================================================
 
-!
 ! From array 'surface' (element, type : node/edge, node(s) ) that describes the
 ! acoustic free surface, determines the points (ixmin, ixmax, izmin and izmax) on the surface
 ! for each element.
