87b1d2ba614ad31031ce039d699cb7736f4f063a
Christina Morency <morency1@llnl.gov>
2008-09-16 14:02:47 +0000

    create_color_image.f90 modified for cluster.

--- create_color_image.f90
+++ create_color_image.f90
@@ -252,7 +252,9 @@
   close(27)
 
 ! open image file and create system command to convert image to more convenient format
-  write(system_command,"('cd OUTPUT_FILES ; convert image',i7.7,'.pnm image',i7.7,'.gif ; rm -f image',i7.7,'.pnm')") it,it,it
+!  write(system_command,"('cd OUTPUT_FILES ; convert image',i7.7,'.pnm image',i7.7,'.gif ; rm -f image',i7.7,'.pnm')") it,it,it
+! for cluster
+  write(system_command,"('cd OUTPUT_FILES ; convert image',i7.7,'.pnm image',i7.7,'.gif')") it,it
 
 ! call the system to convert image to GIF
   call system(system_command)
