3f4d03bde8bbec8369cb4ffa3a9eb173af83e822
Qinya Liu <liuqy@physics.utoronto.ca>
2011-07-20 21:36:27 +0000

    More detailed instructions on how to use initial input plane waves (initialfield=true) an example is checked into EXAMPLES/init_plane prepare_initial_field is modified to allow plane wave incidence from the right (angleforce=negative vals) output snapshots are in jpg files which are easier to convert to avi movie

---
+++ UTILS/convert_gif_to_avi.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for file in imag*.gif; do
+  jfile=`echo $file | sed 's/gif/jpg/'`
+  convert -quality 100 $file $jfile
+  echo convert -quality 100 $file $jfile
+done
+mencoder "mf://imag*.jpg" -mf fps=1 -o output.avi -ovc lavc -lavcopts vcodec=mpeg4
