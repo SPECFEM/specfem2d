69fddc00183b2370580a929e54b76517b9c79a30
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2012-11-26 12:04:24 +0000

    added the original "grep" statement

--- salt_dome/CPML_normal/create_edge_file_to_add_PML_layer.bash
+++ salt_dome/CPML_normal/create_edge_file_to_add_PML_layer.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-gawk '{print $3  "\n" $4}' a | sort -n | uniq > bottom_edge
+# extract left edge information (flag value == 4) from the absorbing surface file
+
+grep ' 4$' modelY1_absorbing_surface_file | gawk '{print $3  "\n" $4}' | sort -n | uniq > left_edge
 
