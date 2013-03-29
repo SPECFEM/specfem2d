2ab0480de897e54954cca33f1ed1c98610beb94d
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2013-03-29 14:57:05 +0000

    added EXAMPLES/CPML_absorbing_layers/CPML_layers_added_to_simple_square_with_Gmsh.geo

--- EXAMPLES/salt_dome/CPML_normal/create_edge_file_to_add_PML_layer.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# extract left edge information (flag value == 4) from the absorbing surface file
-
-grep ' 4$' modelY1_absorbing_surface_file | gawk '{print $3  "\n" $4}' | sort -n | uniq > left_edge
-
