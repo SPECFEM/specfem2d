5259f83b854c2659f4123643d47486c3ed9d1b28
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2013-04-10 19:37:04 +0000

    added "Stacey" to the name of current CPML examples

--- EXAMPLES/Gmsh_example_MPI/process_the_Gmsh_file_once_and_for_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# create the absorbing and free surface files from the Gmsh file
-#
-
-python ../../UTILS/Gmsh/LibGmsh2Specfem_convert_Gmsh_to_Specfem2D_official.py SqrCirc.msh -t F -l A -b A -r A
-
