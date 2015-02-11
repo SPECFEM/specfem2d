25c4a2a3aab22f38a543331128e1104338c38321
bottero <alexis.bottero@gmail.com>
2015-02-11 16:16:43 +0100

    Clean axisym mesh example

--- utils/cubit2specfem2d/simpleAxisym2dMesh.py
+++ utils/cubit2specfem2d/simpleAxisym2dMesh.py
@@ -41,56 +41,62 @@ cubit.cmd('block 1 attribute index 5 0 ')     # Q_flag
 cubit.cmd('block 1 attribute index 6 0 ')     # anisotropy_flag
 cubit.cmd('block 1 element type QUAD4')       # element type. Must be QUAD4 for cubit2specfem2d
 
-### Create PMLs :
+### Create PMLs quads :
 # z pmls :
 cubit.cmd('create element extrude edge in curve 1 direction 0 0 -1 distance '+str(3*elementSize)+' layers '+str(nPml))
 cubit.cmd('create element extrude edge in curve 3 direction 0 0 1 distance '+str(3*elementSize)+' layers '+str(nPml))
 cubit.cmd('block 100 (face all except face in block 1)')
-cubit.cmd('block 100 name "pml_z_elast"')
-cubit.cmd('block 100 attribute count 6')        # number of attributes
-cubit.cmd('block 100 attribute index 1 1')      # material index
-cubit.cmd('block 100 attribute index 2 2500 ')  # vp
-cubit.cmd('block 100 attribute index 3 1000 ')  # vs
-cubit.cmd('block 100 attribute index 4 2200 ')  # rho
-cubit.cmd('block 100 attribute index 5 0 ')     # Q_flag
-cubit.cmd('block 100 attribute index 6 0 ')     # anisotropy_flag
-cubit.cmd('block 100 element type QUAD4')
-
 # x pmls :
 cubit.cmd('create element extrude edge all with x_coord > '+str(lx-elementSize/100.0)+' direction 1 0 0 distance '+str(3*elementSize)+' layers '+str(nPml))
 cubit.cmd('block 101 (face all except face in block 1 100)')
-cubit.cmd('block 101 name "pml_x_elast"')
-cubit.cmd('block 101 attribute count 6')        # number of attributes
-cubit.cmd('block 101 attribute index 1 1')      # material index
-cubit.cmd('block 101 attribute index 2 2500 ')  # vp
-cubit.cmd('block 101 attribute index 3 1000 ')  # vs
-cubit.cmd('block 101 attribute index 4 2200 ')  # rho
-cubit.cmd('block 101 attribute index 5 0 ')     # Q_flag
-cubit.cmd('block 101 attribute index 6 0 ')     # anisotropy_flag
-cubit.cmd('block 101 element type QUAD4')
-
 # xz pmls :
 cubit.cmd('set duplicate block elements on')
-cubit.cmd('block 300 face in block 101 with z_coord > 0')
-cubit.cmd('block 301 face in block 101 with z_coord < '+str(-lz)) 
-cubit.cmd('block 102 face in block 300 301') 
-cubit.cmd('delete block 300 301')
+cubit.cmd('block 200 face in block 101 with z_coord > 0')
+cubit.cmd('block 201 face in block 101 with z_coord < '+str(-lz)) 
+cubit.cmd('block 102 face in block 200 201') 
 #cubit.cmd('block 102 group (face in block 101 with z_coord > 0) and (face in block 101 with z_coord < '+str(-lz)) # Does not work??!!
-cubit.cmd('block 102 name "pml_xz_elast"')
-cubit.cmd('block 102 attribute count 6')        # number of attributes
-cubit.cmd('block 102 attribute index 1 1')      # material index
-cubit.cmd('block 102 attribute index 2 2500 ')  # vp
-cubit.cmd('block 102 attribute index 3 1000 ')  # vs
-cubit.cmd('block 102 attribute index 4 2200 ')  # rho
-cubit.cmd('block 102 attribute index 5 0 ')     # Q_flag
-cubit.cmd('block 102 attribute index 6 0 ')     # anisotropy_flag
-cubit.cmd('block 102 element type QUAD4')
 
 ### Create geometry from free mesh (necessary to merge) :
-cubit.cmd('create mesh geometry Face in block 100 feature_angle 135.0')
-cubit.cmd('create mesh geometry Face in block 101 feature_angle 135.0')
-cubit.cmd('create mesh geometry Face in block 102 feature_angle 135.0')
+cubit.cmd('create mesh geometry face in block 100 feature_angle 135.0')
+cubit.cmd('create mesh geometry face in block 101 feature_angle 135.0')
+cubit.cmd('create mesh geometry face in block 102 feature_angle 135.0')
 cubit.cmd('merge all')
+cubit.cmd('delete block 100 101 102 200 201')
+
+### Create PMLs blocks
+# z pmls :
+cubit.cmd('block 1000 face in surf 2 3)')
+cubit.cmd('block 1000 name "pml_z_elast"')
+cubit.cmd('block 1000 attribute count 6')        # number of attributes
+cubit.cmd('block 1000 attribute index 1 1')      # material index
+cubit.cmd('block 1000 attribute index 2 2500 ')  # vp
+cubit.cmd('block 1000 attribute index 3 1000 ')  # vs
+cubit.cmd('block 1000 attribute index 4 2200 ')  # rho
+cubit.cmd('block 1000 attribute index 5 0 ')     # Q_flag
+cubit.cmd('block 1000 attribute index 6 0 ')     # anisotropy_flag
+cubit.cmd('block 1000 element type QUAD4')
+# x pmls :
+cubit.cmd('block 1001 face in surf 4)')
+cubit.cmd('block 1001 name "pml_x_elast"')
+cubit.cmd('block 1001 attribute count 6')        # number of attributes
+cubit.cmd('block 1001 attribute index 1 1')      # material index
+cubit.cmd('block 1001 attribute index 2 2500 ')  # vp
+cubit.cmd('block 1001 attribute index 3 1000 ')  # vs
+cubit.cmd('block 1001 attribute index 4 2200 ')  # rho
+cubit.cmd('block 1001 attribute index 5 0 ')     # Q_flag
+cubit.cmd('block 1001 attribute index 6 0 ')     # anisotropy_flag
+cubit.cmd('block 1001 element type QUAD4')
+# xz pmls :
+cubit.cmd('block 1002 face in surf 5 6)')
+cubit.cmd('block 1002 name "pml_xz_elast"')
+cubit.cmd('block 1002 attribute count 6')        # number of attributes
+cubit.cmd('block 1002 attribute index 1 1')      # material index
+cubit.cmd('block 1002 attribute index 2 2500 ')  # vp
+cubit.cmd('block 1002 attribute index 3 1000 ')  # vs
+cubit.cmd('block 1002 attribute index 4 2200 ')  # rho
+cubit.cmd('block 1002 attribute index 5 0 ')     # Q_flag
+cubit.cmd('block 1002 attribute index 6 0 ')     # anisotropy_flag
+cubit.cmd('block 1002 element type QUAD4')
 
 ### Creating absorbing surfaces
 cubit.cmd('block 2 edge in curve 4') 
@@ -105,10 +111,10 @@ cubit.cmd('block 4 element type BAR2')
 
 ### Create Axis (must include pmls)
 
-cubit.cmd('block 6 edge with x_coord < '+str(elementSizeSolid/100.0))
-cubit.cmd('block 6 name "elements_axis"')
+cubit.cmd('block 5 edge with x_coord < '+str(elementSize/100.0))
+cubit.cmd('block 5 name "elements_axis"')
+cubit.cmd('block 5 element type BAR2')
 
 ### Remove duplicates and fix problems
-cubit.cmd('block 101 remove face in block 102')
 cubit.cmd('set dev on') # Set development commands on (to be able to delete)
-cubit.cmd('delete face all except face in block all') # Delete all other faces
+cubit.cmd('delete face all except face in block all') # Delete all duplicate faces
