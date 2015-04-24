df4bd65aebacc8e95d298bbeaf5662143b6c01e3
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2015-04-24 21:49:45 +0200

    wrote a script to run the whole example (forward run and then adjoint simulation)

---
+++ EXAMPLES/LuoYang_fluid_solid_kernel/run_a_single_run_of_this_two_step_example.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# script runs mesher and solver (in serial)
+# using this example setup
+#
+
+echo "running example: `date`"
+currentdir=`pwd`
+
+echo
+echo "(will take about 1 minute)"
+echo
+
+# sets up directory structure in current example directoy
+echo
+echo "   setting up example..."
+echo
+
+mkdir -p OUTPUT_FILES
+mkdir -p DATA
+mkdir -p SEM
+
+# sets up local DATA/ directory
+cd DATA/
+rm -f Par_file SOURCE interfaces.dat
+ln -s ../Par_file Par_file
+ln -s ../SOURCE SOURCE
+cp ../interfaces.dat interfaces.dat
+cd ../
+
+# cleans output files (only if a command line argument is NOT given, because the second time when we run the adjoint run we should not erase anything)
+noargument=0
+if [ "$#" -eq "$noargument" ]
+then
+  echo "erasing all database files"
+  rm -rf OUTPUT_FILES/*
+else
+  echo "not erasing any of the database files"
+fi
+
+cd $currentdir
+
+# links executables
+rm -f xmeshfem2D xspecfem2D
+ln -s ../../bin/xmeshfem2D
+ln -s ../../bin/xspecfem2D
+
+# stores setup
+cp -f DATA/Par_file OUTPUT_FILES
+cp -f DATA/SOURCE OUTPUT_FILES
+
+# runs database generation
+echo
+echo "  running mesher..."
+echo
+./xmeshfem2D
+
+# runs simulation
+echo
+echo "  running solver..."
+echo
+./xspecfem2D
+
+# stores output
+cp -f DATA/*SOURCE* DATA/*STATIONS* OUTPUT_FILES
+
+echo
+echo "see results in directory: OUTPUT_FILES/"
+echo
+echo "done"
+date
--- EXAMPLES/LuoYang_fluid_solid_kernel/run_this_example.sh
+++ EXAMPLES/LuoYang_fluid_solid_kernel/run_this_example.sh
@@ -1,63 +1,18 @@
 #!/bin/bash
-#
-# script runs mesher and solver (in serial)
-# using this example setup
-#
 
-echo "running example: `date`"
-currentdir=`pwd`
+# define your Fortran compiler here
+FC="gfortran"
 
-echo
-echo "(will take about 1 minute)"
-echo
+cp -f Par_file_forward Par_file
+./run_a_single_run_of_this_two_step_example.sh
 
-# sets up directory structure in current example directoy
-echo
-echo "   setting up example..."
-echo
+rm -f xadj_source
+$FC adj_source.f90 -o xadj_source
+./xadj_source
+rm -f xadj_source
 
-mkdir -p OUTPUT_FILES
-mkdir -p DATA
-mkdir -p SEM
+cp -f Par_file_adjoint Par_file
+./run_a_single_run_of_this_two_step_example.sh do_not_erase_disk_files
 
-# sets up local DATA/ directory
-cd DATA/
-ln -s ../Par_file Par_file
-ln -s ../SOURCE SOURCE
-cp ../interfaces.dat interfaces.dat
-cd ../
+gnuplot plot_kernel.gnu
 
-# cleans output files
-rm -rf OUTPUT_FILES/*
-
-cd $currentdir
-
-# links executables
-rm -f xmeshfem2D xspecfem2D
-ln -s ../../bin/xmeshfem2D
-ln -s ../../bin/xspecfem2D
-
-# stores setup
-cp DATA/Par_file OUTPUT_FILES/
-cp DATA/SOURCE OUTPUT_FILES/
-
-# runs database generation
-echo
-echo "  running mesher..."
-echo
-./xmeshfem2D
-
-# runs simulation
-echo
-echo "  running solver..."
-echo
-./xspecfem2D
-
-# stores output
-cp DATA/*SOURCE* DATA/*STATIONS* OUTPUT_FILES
-
-echo
-echo "see results in directory: OUTPUT_FILES/"
-echo
-echo "done"
-date
