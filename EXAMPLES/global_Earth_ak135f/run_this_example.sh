dfebde1e5b7f88db91b35013dc0e91e35ad22ab2
bottero <alexis.bottero@gmail.com>
2015-05-07 18:03:27 +0200

    Update run_this_example.sh for example global_earth_ak135f to be able to run in mpi

--- EXAMPLES/global_Earth_ak135f/run_this_example.sh
+++ EXAMPLES/global_Earth_ak135f/run_this_example.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-./make_specific_mesher_for_2D_Earth.csh
+./make_specific_mesher_for_2D_Earth.csh # Build the mesh
 
-mkdir -p OUTPUT_FILES
-../../bin/xmeshfem2D
-../../bin/xspecfem2D
+mkdir -p OUTPUT_FILES # Will contains the results of the simulation
 
+../../bin/xmeshfem2D # Runs the mesher (to create the database)
+
+# Get the number of processors from Par_file
+NPROC=`grep nproc DATA/Par_file | cut -d = -f 2 | cut -d \# -f 1 | tr -d ' '`
+
+if [ "$NPROC" -eq 1 ]; then # If nproc == 1 this is a serial simulation
+  echo
+  echo " Running solver..."
+  echo
+  ./xspecfem2D
+else # Else this is a MPI simulation
+  echo
+  echo " Running solver on $NPROC processors..."
+  echo
+  mpirun -np $NPROC ../../bin/xspecfem2D
+fi
