14df0294210d79895c44b727a2ad5a1a4bd3c934
bottero <alexis.bottero@gmail.com>
2015-06-03 14:42:08 +0200

    Adapt locate_source_moment_tensor to AXISYM

--- src/specfem2D/locate_source_moment_tensor.F90
+++ src/specfem2D/locate_source_moment_tensor.F90
@@ -50,6 +50,8 @@
                ispec_selected_source,is_proc_source,nb_proc_source,nproc,myrank, &
                xi_source,gamma_source,coorg,knods,ngnod,npgeo)
 
+  use specfem_par, only : AXISYM,is_on_the_axis,xiglj
+
 #ifdef USE_MPI
   use mpi
 #endif
@@ -173,7 +175,16 @@
 ! ****************************************
 
 ! use initial guess in xi and gamma
-  xi = xigll(ix_initial_guess)
+
+  if (AXISYM) then
+    if (is_on_the_axis(ispec_selected_source)) then
+      xi = xiglj(ix_initial_guess)
+    else
+      xi = xigll(ix_initial_guess)
+    endif
+  else
+    xi = xigll(ix_initial_guess)
+  endif
   gamma = zigll(iz_initial_guess)
 
 ! iterate to solve the non linear system
