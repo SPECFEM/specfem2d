ddc64d06a17850a347f67838c0cc15a480af4fa5
bottero <alexis.bottero@gmail.com>
2015-06-18 15:01:42 +0200

    Fix a bug with attenuation in external models

--- src/specfem2D/define_external_model_from_tomo_file.f90
+++ src/specfem2D/define_external_model_from_tomo_file.f90
@@ -313,6 +313,7 @@ subroutine define_external_model_from_tomo_file()
 ! ----------------------------------------------------------------------------------------
 
   use specfem_par, only: tomo_material,coord,nspec,ibool,kmato,rhoext,vpext,vsext, &
+                       QKappa_attenuation,Qmu_attenuation,anisotropy, &
                        QKappa_attenuationext,Qmu_attenuationext,poroelastcoef,density, &
                        c11ext,c13ext,c15ext,c33ext,c35ext,c55ext,c12ext,c23ext,c25ext
 
@@ -353,25 +354,38 @@ subroutine define_external_model_from_tomo_file()
            c12ext(i,j,ispec) = 0.d0
            c23ext(i,j,ispec) = 0.d0
            c25ext(i,j,ispec) = 0.d0
-           !! AB AB : The 3 following lines are important, otherwise PMLs won't work :
+           !! AB AB : The 3 following lines are important, otherwise PMLs won't work. This should be fixed TODO
+           !! (we assign these values several times: indeed for each kmato(ispec) it can exist a lot of rhoext(i,j,ispec) )
            density(1,kmato(ispec)) = rhoext(i,j,ispec)
            poroelastcoef(3,1,kmato(ispec)) = rhoext(i,j,ispec) * vpext(i,j,ispec) * vpext(i,j,ispec)
            poroelastcoef(2,1,kmato(ispec)) =  rhoext(i,j,ispec) * vsext(i,j,ispec) * vsext(i,j,ispec)
-         else
+           !! AB AB : I do the same with anisotropy and attenuation even if I don't use them (for the future) :
+           anisotropy(1,kmato(ispec)) = c11ext(i,j,ispec)
+           anisotropy(2,kmato(ispec)) = c13ext(i,j,ispec)
+           anisotropy(3,kmato(ispec)) = c15ext(i,j,ispec)
+           anisotropy(4,kmato(ispec)) = c33ext(i,j,ispec)
+           anisotropy(5,kmato(ispec)) = c35ext(i,j,ispec)
+           anisotropy(6,kmato(ispec)) = c55ext(i,j,ispec)
+           anisotropy(7,kmato(ispec)) = c12ext(i,j,ispec)
+           anisotropy(8,kmato(ispec)) = c23ext(i,j,ispec)
+           anisotropy(9,kmato(ispec)) = c25ext(i,j,ispec)
+           QKappa_attenuation(kmato(ispec)) = QKappa_attenuationext(i,j,ispec)
+           Qmu_attenuation(kmato(ispec)) = Qmu_attenuationext(i,j,ispec)
+         else ! Internal model
            rhoext(i,j,ispec) = density(1,kmato(ispec))
            vpext(i,j,ispec) = sqrt(poroelastcoef(3,1,kmato(ispec))/rhoext(i,j,ispec))
            vsext(i,j,ispec) = sqrt(poroelastcoef(2,1,kmato(ispec))/rhoext(i,j,ispec))
-           QKappa_attenuationext(i,j,ispec) = 9999. ! this means no attenuation
-           Qmu_attenuationext(i,j,ispec)    = 9999. ! this means no attenuation
-           c11ext(i,j,ispec) = 0.d0   ! this means no anisotropy
-           c13ext(i,j,ispec) = 0.d0
-           c15ext(i,j,ispec) = 0.d0
-           c33ext(i,j,ispec) = 0.d0
-           c35ext(i,j,ispec) = 0.d0
-           c55ext(i,j,ispec) = 0.d0
-           c12ext(i,j,ispec) = 0.d0
-           c23ext(i,j,ispec) = 0.d0
-           c25ext(i,j,ispec) = 0.d0
+           QKappa_attenuationext(i,j,ispec) = QKappa_attenuation(kmato(ispec))
+           Qmu_attenuationext(i,j,ispec) = Qmu_attenuation(kmato(ispec))
+           c11ext(i,j,ispec) = anisotropy(1,kmato(ispec))
+           c13ext(i,j,ispec) = anisotropy(2,kmato(ispec))
+           c15ext(i,j,ispec) = anisotropy(3,kmato(ispec))
+           c33ext(i,j,ispec) = anisotropy(4,kmato(ispec))
+           c35ext(i,j,ispec) = anisotropy(5,kmato(ispec))
+           c55ext(i,j,ispec) = anisotropy(6,kmato(ispec))
+           c12ext(i,j,ispec) = anisotropy(7,kmato(ispec))
+           c23ext(i,j,ispec) = anisotropy(8,kmato(ispec))
+           c25ext(i,j,ispec) = anisotropy(9,kmato(ispec))
          endif
       enddo
     enddo
