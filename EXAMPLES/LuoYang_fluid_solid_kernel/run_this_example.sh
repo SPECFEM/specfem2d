ee2e6db3f6ad597389fb5e814218a25e34d9b6d7
daniel peter <daniel.peter22@gmail.com>
2016-01-06 19:01:07 +0300

    updates example LuoYang_fluid_solid_kernel

--- EXAMPLES/LuoYang_fluid_solid_kernel/run_this_example.sh
+++ EXAMPLES/LuoYang_fluid_solid_kernel/run_this_example.sh
@@ -3,16 +3,20 @@
 # define your Fortran compiler here
 FC="gfortran"
 
-cp -f Par_file_forward Par_file
+# forward simulation
+./change_simulation_type.pl -F
 ./run_a_single_run_of_this_two_step_example.sh
 
+# adjoint source
 rm -f xadj_source
 $FC adj_source.f90 -o xadj_source
 ./xadj_source
 rm -f xadj_source
 
-cp -f Par_file_adjoint Par_file
+# adjoint kernel simulation
+./change_simulation_type.pl -b
 ./run_a_single_run_of_this_two_step_example.sh do_not_erase_disk_files
 
+# kernel images
 gnuplot plot_kernel.gnu
 
