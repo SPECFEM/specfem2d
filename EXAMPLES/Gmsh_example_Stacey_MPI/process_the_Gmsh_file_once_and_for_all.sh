2fbb9a0e0d637917e4500f7a0325cd1cc5852bbc
daniel peter <daniel.peter22@gmail.com>
2016-06-20 01:08:24 +0300

    moves mesh files to MESH/ folder in Gmsh_example_Stacey_MPI example

--- EXAMPLES/Gmsh_example_Stacey_MPI/process_the_Gmsh_file_once_and_for_all.sh
+++ EXAMPLES/Gmsh_example_Stacey_MPI/process_the_Gmsh_file_once_and_for_all.sh
@@ -2,6 +2,9 @@
 #
 # create the absorbing and free surface files from the Gmsh file
 #
+cd MESH/
 
 python ../../utils/Gmsh/LibGmsh2Specfem_convert_Gmsh_to_Specfem2D_official.py SqrCirc.msh -t F -l A -b A -r A
+
+cd ../
 
