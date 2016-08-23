0cd5220db5b19bdc4e481266b30752ea5b321faa
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2016-08-23 22:11:04 +0200

    fixed a typo

--- EXAMPLES/change_something_in_all_the_Par_files_automatically.bash
+++ EXAMPLES/change_something_in_all_the_Par_files_automatically.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# If you want to change the name of a parameter or anything else in all the Par_files below in an automatic way
+# If you want to change the name of a parameter or anything else in all the Par_files in an automatic way
 # you can also use a "find" command combined with a "sed" command, below is an example.
 #
 # However this will *not* make the change in ../DATA/Par_file, which you will need to do separately.
