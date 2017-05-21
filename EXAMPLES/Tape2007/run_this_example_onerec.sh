d4a5de4e64716253a49921b1c706af8d6b68b74d
Daniel Peter <daniel.peter22@gmail.com>
2017-05-21 15:48:51 +0300

    updates example Tape2007/, taking checkerboard simulation as default; added travis test

--- EXAMPLES/Tape2007/run_this_example_onerec.sh
+++ EXAMPLES/Tape2007/run_this_example_onerec.sh
@@ -24,18 +24,10 @@ rm -rf OUTPUT_FILES/*
 # sets up local DATA/ directory
 cd DATA/
 rm -f Par_file
-ln -s Par_file_Tape2007_132rec_checker Par_file
+ln -s Par_file_Tape2007_onerec Par_file
 rm -f SOURCE
-ln -s SOURCE_005 SOURCE
-rm -f proc000000_model_velocity.dat_input
-ln -s model_velocity.dat_checker proc000000_model_velocity.dat_input
-rm -f STATIONS
-ln -s STATIONS_checker STATIONS
+ln -s SOURCE_001 SOURCE
 cd ../
-
-# compiles executables in root directory
-#cd ../../
-#make > tmp.log
 
 cd $currentdir
 
@@ -50,38 +42,46 @@ cp DATA/SOURCE OUTPUT_FILES/
 
 # Get the number of processors
 NPROC=`grep ^NPROC DATA/Par_file | cut -d = -f 2 | cut -d \# -f 1 | tr -d ' '`
-if [ "$NPROC" -ne 1 ]; then
-  echo "Checkerboard example requires to run simulation with a single process only."
-  echo "Please set NPROC to 1 in DATA/Par_file"
-  exit 1
-fi
 
 # runs database generation
-# This is a serial simulation
-echo
-echo "running mesher..."
-echo
-./xmeshfem2D
+if [ "$NPROC" -eq 1 ]; then
+  # This is a serial simulation
+  echo
+  echo "running mesher..."
+  echo
+  ./xmeshfem2D
+else
+  # This is a MPI simulation
+  echo
+  echo "running mesher on $NPROC processors..."
+  echo
+  mpirun -np $NPROC ./xmeshfem2D
+fi
 # checks exit code
 if [[ $? -ne 0 ]]; then exit 1; fi
 
 # runs simulation
-# This is a serial simulation
-echo
-echo "running solver..."
-echo
-./xspecfem2D
+if [ "$NPROC" -eq 1 ]; then
+  # This is a serial simulation
+  echo
+  echo "running solver..."
+  echo
+  ./xspecfem2D
+else
+  # This is a MPI simulation
+  echo
+  echo "running solver on $NPROC processors..."
+  echo
+  mpirun -np $NPROC ./xspecfem2D
+fi
 # checks exit code
 if [[ $? -ne 0 ]]; then exit 1; fi
 
 # stores output
-cp DATA/SOURCE OUTPUT_FILES/
-cp DATA/STATIONS OUTPUT_FILES/
-#cp DATA/STATIONS_checker OUTPUT_FILES/
+cp DATA/SOURCE DATA/STATIONS OUTPUT_FILES
 
 echo
 echo "see results in directory: OUTPUT_FILES/"
 echo
 echo "done"
 date
-
