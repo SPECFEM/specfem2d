87abbc8b96e431fb6cbcd194faec40e2e9919980
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2018-03-12 12:26:37 +0100

    removed EXAMPLES/unresolved_bugs/bugSource (the code was right; it was a near-field term, not a bug)

--- EXAMPLES/unresolved_bugs/bugSource/show_bug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-
-./run_this_example.sh # Run specfem2d
-eog OUTPUT_FILES/image0001800.jpg &
-./plot.py OUTPUT_FILES/AA.S0001.PRE.semp OUTPUT_FILES/AA.S0003.PRE.semp --hold -n --shift -0.05 --xlabel "Shifted and normalized signals at source and first receiver" &
-./plot.py OUTPUT_FILES/AA.S0003.PRE.semp OUTPUT_FILES/AA.S0002.PRE.semp --hold -n --shift -0.05 --xlabel "Shifted and normalized signals at first and second receiver" &
-
