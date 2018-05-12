769765b3745831b61d42b7e9935f10361944bb19
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2018-05-13 01:46:40 +0200

    added EXAMPLES/attenuation/viscoelastic/Par_file_attenuation_2D_source_in_point_inside_an_element and EXAMPLES/attenuation/viscoelastic/SOURCE_attenuation_2D_source_in_point_inside_an_element

--- EXAMPLES/attenuation/viscoelastic/run_this_example.sh
+++ EXAMPLES/attenuation/viscoelastic/run_this_example.sh
@@ -17,9 +17,57 @@ mkdir -p DATA
 
 # sets up local DATA/ directory
 cd DATA/
-cp ../Par_file_attenuation_2D Par_file
+
+# use this to test a source that is located at a corner point shared between four spectral elements
+cp ../Par_file_attenuation_2D_source_in_point_shared_by_four_elements Par_file
+cp ../SOURCE_attenuation_2D_source_in_point_shared_by_four_elements SOURCE
+
+# or use this to test a source that is located inside a given element, which does NOT coincide with a GLL point
+# cp ../Par_file_attenuation_2D_source_in_point_inside_an_element Par_file
+# cp ../SOURCE_attenuation_2D_source_in_point_inside_an_element SOURCE
+
 cp ../interfaces_attenuation_analytic.dat .
-cp ../SOURCE_attenuation_2D SOURCE
+cd ../
+
+# cleans output files
+rm -rf OUTPUT_FILES/*
+
+cd $currentdir
+
+# links executables
+rm -f xmeshfem2D xspecfem2D
+ln -s ../../../bin/xmeshfem2D
+ln -s ../../../bin/xspecfem2D
+
+# stores setup
+cp DATA/Par_file OUTPUT_FILES/
+cp DATA/SOURCE OUTPUT_FILES/
+
+# runs database generation
+echo
+echo "  running mesher..."
+echo
+./xmeshfem2D
+
+# runs simulation
+echo
+echo "  running solver..."
+echo
+./xspecfem2D
+
+# stores output
+cp DATA/*SOURCE* DATA/*STATIONS* OUTPUT_FILES
+
+echo
+echo "see results in directory: OUTPUT_FILES/"
+echo
+echo "done"
+date
+# use this to test a source that is located at a corner point shared between four spectral elements
+cp ../Par_file_attenuation_2D_source_in_point_shared_by_four_elements Par_file
+cp ../SOURCE_attenuation_2D_source_in_point_shared_by_four_elements SOURCE
+
+cp ../interfaces_attenuation_analytic.dat .
 cd ../
 
 # cleans output files
