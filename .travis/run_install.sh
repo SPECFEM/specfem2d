68e521f5e943e1851355f22737818d66975f8812
Daniel Peter <daniel.peter22@gmail.com>
2019-02-09 14:46:41 +0300

    updates travis scripts

---
+++ .travis/run_install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# fortran/openMPI compiler
+sudo apt-get install gfortran libgomp1 openmpi-bin libopenmpi-dev
+
+# python script needs numpy
+echo "Python on path: `which python`"
+echo "pip on path: $(which pip)"
+# for distribution precise
+#sudo apt-get install -qq python-numpy #python-scipy
+# trusty:
+# (Sep2017 update: adding flag --user : see https://github.com/travis-ci/travis-ci/issues/8382)
+pip install --user --upgrade pip setuptools wheel
+pip install --user --only-binary=numpy numpy
+# version info
+python --version
+
+# version infos
+echo "compiler versions:" ${FC} ${MPIFC} ${CC}
+${FC} --version
+${MPIFC} --version
+${CC} --version
+
+# installs the CUDA toolkit
+if [ "$CUDA" == "true" ]; then
+  echo "Installing CUDA library version"
+  echo "CUDA version: ${CUDA_VERSION}"
+  # note: travis could stall and time out here
+  #       one could try to add: travis_retry sudo dpgk -i ..
+  #       https://docs.travis-ci.com/user/common-build-problems/#travis_retry
+  #
+  # remove old nvidia-cuda packages
+  #sudo apt-get remove nvidia-cuda-* ;
+  # gets packages
+  ## distribution trusty: from ubuntu 14.04
+  wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA_VERSION}_amd64.deb
+  sudo dpkg -i cuda-repo-ubuntu1404_${CUDA_VERSION}_amd64.deb
+  ## distribution precise: from ubuntu 12.04
+  #wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_${CUDA_VERSION}_amd64.deb;
+  #sudo dpkg -i cuda-repo-ubuntu1204_${CUDA_VERSION}_amd64.deb;
+  # update
+  echo "Updating libraries"
+  sudo apt-get update -qq
+  dpkg -l | grep cuda
+  export CUDA_APT=${CUDA_VERSION:0:3}
+  export CUDA_APT=${CUDA_APT/./-}
+  # installs packages
+  # CUDA_PACKAGES="cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT}";
+  CUDA_PACKAGES="cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}"
+  echo "Installing ${CUDA_PACKAGES}"
+  sudo apt-get install -y ${CUDA_PACKAGES}
+  sudo apt-get clean
+  export CUDA_HOME=/usr/local/cuda-${CUDA_VERSION:0:3}
+  export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
+  export PATH=${CUDA_HOME}/bin:${PATH}
+  echo ""
+  nvcc --version
+else
+  export CUDA_HOME=""
+fi
+
+# storing updated environment parameters for following bash-script
+echo "export PATH=${PATH}" > $HOME/.tmprc
+echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> $HOME/.tmprc
+echo "export CUDA_HOME=${CUDA_HOME}" >> $HOME/.tmprc
+
+echo "exports:"
+export
+echo ""
---
+++ .travis/run_tests.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+
+# getting updated environment (CUDA_HOME, PATH, ..)
+if [ -f $HOME/.tmprc ]; then source $HOME/.tmprc; fi
+
+###########################################################
+# setup
+###########################################################
+# chooses example directory
+case "$TESTDIR" in
+0) dir=./ ;;
+1) dir=EXAMPLES/simple_topography_and_also_a_simple_fluid_layer/ ;;
+2) dir=EXAMPLES/semi_infinite_homogeneous/ ;;
+3) dir=EXAMPLES/Gmsh_example_Stacey_MPI/ ;;
+4) dir=EXAMPLES/Tromp2005_kernel/ ;;
+5) dir=EXAMPLES/poroelastic_acoustic/ ;;
+6) dir=EXAMPLES/noise_uniform/ ;;
+7) dir=EXAMPLES/axisymmetric_case_AXISYM_option/ ;;
+8) dir=EXAMPLES/simple_topography_and_also_a_simple_fluid_layer/ ;;
+9) dir=EXAMPLES/Industrial_Format_SEP/ ;;
+10) dir=EXAMPLES/Rayleigh_wave_no_crack/ ;;
+11) dir=EXAMPLES/Rayleigh_wave_with_crack/ ;;
+12) dir=EXAMPLES/Tape2007/ ;;
+*) dir=EXAMPLES/simple_topography_and_also_a_simple_fluid_layer/ ;;
+esac
+
+
+# info
+echo $TRAVIS_BUILD_DIR
+echo $WORKDIR
+echo
+echo "**********************************************************"
+echo
+echo "configuration test: TESTID=${TESTID} TESTDIR=${TESTDIR} TESTCOV=${TESTCOV} TESTFLAGS=${TESTFLAGS}"
+echo
+echo "    test directory: $dir"
+echo
+echo "**********************************************************"
+echo
+
+# bash function for checking seismogram output with reference solutions
+my_test(){
+  echo "testing seismograms:"
+  ln -s $WORKDIR/utils/compare_seismogram_correlations.py
+  ./compare_seismogram_correlations.py REF_SEIS/ OUTPUT_FILES/
+  if [[ $? -ne 0 ]]; then exit 1; fi
+  ./compare_seismogram_correlations.py REF_SEIS/ OUTPUT_FILES/ | grep min/max | cut -d \| -f 3 | awk '{print "correlation:",$1; if ($1 < 0.9 ){print $1,"failed"; exit 1;}else{ print $1,"good"; exit 0;}}'
+  if [[ $? -ne 0 ]]; then exit 1; fi
+  rm -rf OUTPUT_FILES/
+}
+
+# bash function for checking seismogram output with reference solutions
+my_report(){
+  # report example
+  if [ -f OUTPUT_FILES/output_mesher.txt ]; then cat OUTPUT_FILES/output_mesher.txt; else exit 1; fi
+  if [ -f OUTPUT_FILES/output_solver.txt ]; then cat OUTPUT_FILES/output_solver.txt; else exit 1; fi
+}
+
+# ASCII kernel files comparison
+my_test_kernel(){
+  echo "testing kernels:"
+  ln -s $WORKDIR/utils/compare_kernel_correlations.py
+  ./compare_kernel_correlations.py OUTPUT_FILES/proc000000_rhop_alpha_beta_kernel.dat REF_KERNEL/proc000000_rhop_alpha_beta_kernel.dat
+  if [[ $? -ne 0 ]]; then exit 1; fi
+  ./compare_kernel_correlations.py OUTPUT_FILES/proc000000_rhop_alpha_beta_kernel.dat REF_KERNEL/proc000000_rhop_alpha_beta_kernel.dat | grep min/max | cut -d \| -f 3 | awk '{print "correlation:",$1; if ($1 < 0.9 ){print $1,"failed"; exit 1;}else{ print $1,"good"; exit 0;}}'
+  if [[ $? -ne 0 ]]; then exit 1; fi
+  rm -rf OUTPUT_FILES/
+}
+
+
+###########################################################
+# configuration & compilation
+###########################################################
+# configuration
+echo 'Configure...' && echo -en 'travis_fold:start:configure\\r'
+echo "configuration:"
+
+if [ "$TESTCOV" == "1" ]; then
+  echo "configuration: for coverage"
+  ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS} FLAGS_CHECK="-fprofile-arcs -ftest-coverage -O0" CFLAGS="-coverage -O0"
+else
+  if [ "$CUDA" == "true" ]; then
+    if [ "$OPENCL" == "true" ]; then
+      echo "configuration: for opencl" # uses libOpenCL provided from CUDA package
+      ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS} OCL_CPU_FLAGS="-g -Wall -std=c99 -DWITH_MPI" OCL_GPU_FLAGS="-Werror" OCL_INC="${CUDA_HOME}/include" OCL_LIB="${CUDA_HOME}/lib64" OCL_LIBS="-lOpenCL"
+    else
+      echo "configuration: for cuda"
+      ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS} CUDA_LIB="${CUDA_HOME}/lib64" CUDA_INC="${CUDA_HOME}/include" CUDA_FLAGS="-Xcompiler -Wall,-Wno-unused-function,-Wno-unused-const-variable,-Wfatal-errors -g -G"
+    fi
+  else
+    echo "configuration: default"
+    ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS}
+  fi
+fi
+
+# (default already) we output to console
+#sed -i "s:IMAIN .*:IMAIN = ISTANDARD_OUTPUT:" setup/constants.h
+
+# Rayleigh wave and manual crack
+if [ "$TESTID" == "17" ]; then
+  sed -i "s:FAST_NUMBERING .*:FAST_NUMBERING = .false.:" setup/constants.h
+  sed -i "s:ADD_A_SMALL_CRACK_IN_THE_MEDIUM .*:ADD_A_SMALL_CRACK_IN_THE_MEDIUM = .true.:" setup/constants.h
+fi
+
+echo -en 'travis_fold:end:configure\\r'
+
+# compilation  (only cleaning)
+echo 'Build...' && echo -en 'travis_fold:start:build\\r'
+echo "compilation:"
+make clean; make -j2 all
+echo -en 'travis_fold:end:build\\r'
+
+
+###########################################################
+# test examples
+###########################################################
+# testing internal mesher example (short & quick for all configuration)
+echo 'Tests...' && echo -en 'travis_fold:start:tests\\r'
+# runs test
+echo "test directory: $dir"
+echo
+cd $dir
+if [ "$TESTID" == "2" ]; then
+  # runs default tests
+  make tests
+else
+  # specific example settings needed for testing
+  if [ "$TESTDIR" == "5" ]; then
+    sed -i "s:^NSTEP .*:NSTEP    = 1000:" DATA/Par_file
+  fi
+  if [ "$TESTDIR" == "7" ]; then
+    sed -i "s:^NPROC .*:NPROC    = 2:" DATA/Par_file
+    sed -i "s:^NSTEP .*:NSTEP    = 1400:" DATA/Par_file
+  fi
+  if [ "$TESTDIR" == "9" ]; then
+    sed -i "s:^NPROC .*:NPROC    = 2:" DATA/Par_file
+    sed -i "s:^NSTEP .*:NSTEP    = 3000:" DATA/Par_file
+  fi
+
+  # coverage run
+  if [ "$TESTCOV" == "1" ]; then
+    sed -i "s:^NSTEP .*:NSTEP    = 400:" DATA/Par_file
+  fi
+
+  # default
+  if [ "$TESTDIR" == "4" ]; then
+    # kernel script
+    ./run_this_example_kernel.sh
+    my_test_kernel
+  else
+    # default script
+    ./run_this_example.sh
+    my_test
+  fi
+  cd $WORKDIR
+fi
+
+
+echo -en 'travis_fold:end:tests\\r'
+
+
+# code coverage: https://codecov.io/gh/geodynamics/specfem2d/
+# additional runs for coverage
+#
+# note: log becomes too long, trying to fold each test output
+echo 'Coverage...' && echo -en 'travis_fold:start:coverage.additional\\r'
+if [ "$TESTCOV" == "1" ] && [ "$TESTID" == "1" ]; then
+  ##
+  ## testing example with pml (longer testing only together with mpi and code coverage)
+  ##
+  cd EXAMPLES/semi_infinite_homogeneous/
+  sed -i "s:^NPROC .*:NPROC    = 2:" DATA/Par_file
+  sed -i "s:^NSTEP .*:NSTEP    = 500:" DATA/Par_file
+  ./run_this_example.sh
+  my_test
+  cd $WORKDIR
+
+  ##
+  ## testing external mesher example with mpi and stacey
+  ##
+  cd EXAMPLES/Gmsh_example_Stacey_MPI/
+  sed -i "s:^NSTEP .*:NSTEP    = 500:" DATA/Par_file
+  ./run_this_example.sh
+  # tests mesh quality output
+  awk '{if(NR==1){dy=sqrt(($2-13.3242693)^2);if(dy>1.e-5){print $0,"failed",dy;exit 1;}else{print $0,"good",dy;exit 0;}}}' OUTPUT_FILES/mesh_quality_histogram.txt
+  if [[ $? -ne 0 ]]; then exit 1; fi
+  my_test
+  cd $WORKDIR
+
+  ##
+  ## testing kernel example
+  ##
+  cd EXAMPLES/Tromp2005_kernel/
+  sed -i "s:^NSTEP .*:NSTEP    = 500:" DATA/Par_file
+  ./run_this_example_kernel.sh
+  # no kernel value testing: only execution failure
+  #my_test_kernel
+  cd $WORKDIR
+
+  ##
+  ## testing poroelastic example
+  ##
+  cd EXAMPLES/poroelastic_acoustic/
+  sed -i "s:^NSTEP .*:NSTEP    = 10:" DATA/Par_file
+  ./run_this_example.sh
+  # only for coverage, comparison would fail: my_test
+  cd $WORKDIR
+
+  ##
+  ## testing noise example
+  ##
+  cd EXAMPLES/noise_uniform/
+  sed -i "s:^NSTEP .*:NSTEP    = 10:" DATA/Par_file_noise_1
+  ./run_this_example.sh
+  # only for coverage, comparison would fail: my_test
+  cd $WORKDIR
+
+  ##
+  ## testing axisymmetric example
+  ##
+  cd EXAMPLES/axisymmetric_case_AXISYM_option/
+  sed -i "s:^NPROC .*:NPROC    = 2:" DATA/Par_file
+  sed -i "s:^NSTEP .*:NSTEP    = 10:" DATA/Par_file
+  ./run_this_example.sh
+  # only for coverage, comparison would fail: my_test
+  cd $WORKDIR
+
+  ##
+  ## testing PML & MPI example
+  ##
+  cd EXAMPLES/simple_topography_and_also_a_simple_fluid_layer/
+  sed -i "s:^NSTEP .*:NSTEP    = 10:" DATA/Par_file
+  ./run_this_example.sh
+  # only for coverage, comparison would fail: my_test
+  cd $WORKDIR
+
+  ##
+  ## testing MPI, SEP example
+  ##
+  cd EXAMPLES/Industrial_Format_SEP/
+  sed -i "s:^NSTEP .*:NSTEP    = 10:" DATA/Par_file
+  ./run_this_example.sh
+  # only for coverage, comparison would fail: my_test
+  cd $WORKDIR
+
+  ##
+  ## testing plane wave example
+  ##
+  cd EXAMPLES/Rayleigh_wave_no_crack/
+  sed -i "s:^NSTEP .*:NSTEP    = 10:" DATA/Par_file
+  # coarser resolution
+  sed -i "s:60:15:g" DATA/Par_file
+  sed -i "s:28:7:g" DATA/Par_file
+  sed -i "s:28:7:g" DATA/interfaces_Rayleigh_flat.dat
+  ./run_this_example.sh
+  # only for coverage, comparison would fail: my_test
+  cd $WORKDIR
+
+  ##
+  ## testing Tape2007 example
+  ##
+  cd EXAMPLES/Tape2007/
+  sed -i "s:^NSTEP .*:NSTEP    = 10:" DATA/Par_file
+  ./run_this_example.sh
+  # only for coverage, comparison would fail: my_test
+  cd $WORKDIR
+fi
+echo -en 'travis_fold:end:coverage.additional\\r'
+
+
+# done
+echo "done `pwd`"
+
